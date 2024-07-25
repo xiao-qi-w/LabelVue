57f2acaee3caaec22ca62294af11bd0bc1df74c8
Richard <1641070557@qq.com>
2024-07-25 16:51:43 +0800

    缩放和拖拽改动，部分实现

--- src/components/ImageMarker.vue
+++ src/components/ImageMarker.vue
@@ -13,7 +13,7 @@
         </el-button>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="createRect = !createRect" :disabled="createRect">
+        <el-button type="primary" @click="handleCreate" :disabled="drawing">
           <i class="el-icon-collection-tag"/>创建标记
         </el-button>
       </el-form-item>
@@ -48,7 +48,7 @@
               @mousedown="handleMouseDown"
               @mousemove="handleMouseMove"
               @mouseup="handleMouseUp"
-              :style="`${!createRect ? '' : 'cursor: crosshair'}`"/>
+              :style="`${!drawing ? '' : 'cursor: crosshair'}`"/>
       <el-dialog width="20vw" title="标签命名">
         <el-form ref="tag">
           <el-form-item>
@@ -82,6 +82,8 @@
       </el-card>
       <el-card class="path-list">
         <h3>图片列表</h3>
+        <p>x: {{this.prevX}}</p>
+        <p>y: {{this.prevY}}</p>
         <!--        <ul class="tag-list">
                   <li v-for="(file, i) in files"
                       :key="i"
@@ -99,7 +101,7 @@
 import Rect from '@/models/Rect'; // 导入类
 
 export default {
-  name: "TestOk",
+  name: "ImageMarker",
   data() {
     return {
       /* 图片相关 */
@@ -108,24 +110,32 @@ export default {
         require('E:\\郭家旗\\Pictures\\Saved Pictures\\bay.jpg'),
         require('E:\\郭家旗\\Pictures\\Saved Pictures\\dusk.jpg')
       ],
+      /* 辅助变量 */
       currentImageIndex: 0,
+      drawing: false,
       /* 缩放相关 */
       dpr: 1,
-      scale: 1,
+      scale: 0,
       maxScale: 3.0,
       minScale: 0.5,
       scaleStep: 0.1,
+      // 鼠标上一刻所在位置
+      prevX: 0,
+      prevY: 0,
       /* 缓存 */
       image: null,
       currentRect: null,
+      canvas: null,
       bufferCanvas: null,
-      bufferCtx: null,
-      createRect: false,
       rects: [], // 保存所有绘制的矩形
     };
   },
   mounted() {
     this.dpr = window.devicePixelRatio || 1;
+    // 获取画布
+    this.canvas = this.$refs.canvas;
+    // 创建离屏画布
+    this.bufferCanvas = document.createElement('canvas');
     this.loadImage();
     // 添加浏览器窗口大小事件
     window.addEventListener("resize", this.loadImage);
@@ -153,85 +163,133 @@ export default {
     },
     // 设置画布大小
     setSize() {
-      // 获取 canvas 和其所在容器元素
-      const canvas = this.$refs.canvas;
+      // 获取画布所在容器元素
       const container = this.$refs.marking;
-      const width = container.clientWidth * this.dpr;
-      const height = container.clientHeight * this.dpr;
-      // 设置宽高
-      canvas.width = width;
-      canvas.height = height;
-      canvas.style.width = `${width / this.dpr}px`;
-      canvas.style.height = `${height / this.dpr}px`;
-      // 创建离屏画布
-      this.bufferCanvas = document.createElement('canvas');
-      this.bufferCanvas.width = canvas.width;
-      this.bufferCanvas.height = canvas.height;
-      this.bufferCanvas.style.width = `${width / this.dpr}px`;
-      this.bufferCanvas.style.height = `${height / this.dpr}px`;
-      this.bufferCtx = this.bufferCanvas.getContext('2d');
+      // 未设置缩放倍率
+      if(this.scale === 0) {
+        // 获取所在容器宽高
+        const width = container.clientWidth * this.dpr;
+        const height = container.clientHeight * this.dpr;
+        // 计算缩放比例
+        const scaleX = width / this.image.width;
+        const scaleY = height / this.image.height;
+        this.scale = Math.min(scaleX, scaleY);
+        this.minScale = this.scale;
+      }
+      // 计算缩放后的图片尺寸
+      const scaledWidth = this.image.width * this.scale;
+      const scaledHeight = this.image.height * this.scale;
+      // 设置画布宽高
+      this.canvas.width = scaledWidth;
+      this.canvas.height = scaledHeight;
+      this.canvas.style.width = `${scaledWidth / this.dpr}px`;
+      this.canvas.style.height = `${scaledHeight / this.dpr}px`;
+      // 设置离屏画布宽高
+      this.bufferCanvas.width = scaledWidth;
+      this.bufferCanvas.height = scaledHeight;
+      this.bufferCanvas.style.width = `${scaledWidth / this.dpr}px`;
+      this.bufferCanvas.style.height = `${scaledHeight / this.dpr}px`;
     },
     // 绘制画布
     drawCanvas() {
-      const canvas = this.$refs.canvas;
-      const ctx = canvas.getContext("2d");
-      // 计算缩放比例
-      const scaleX = canvas.width / (this.image.width / this.dpr);
-      const scaleY = canvas.height / (this.image.height / this.dpr);
-      this.scale = Math.min(scaleX, scaleY);
-      // 计算缩放后的图片尺寸
-      const scaledWidth = (this.image.width / this.dpr) * this.scale;
-      const scaledHeight = (this.image.height / this.dpr) * this.scale;
-      // 获取容器宽高
-      const container = this.$refs.marking;
-      const width = container.clientWidth * this.dpr;
-      const height = container.clientHeight * this.dpr;
-      // 图片相对于所在容器左上角偏移量
-      const offsetX = (width - scaledWidth) / 2;
-      const offsetY = (height - scaledHeight) / 2;
+      const ctx = this.canvas.getContext('2d');
+      const bufferCtx = this.bufferCanvas.getContext('2d');
+      const width = this.canvas.width;
+      const height = this.canvas.height;
       // 绘制缩放后的图片到离屏画布
-      this.bufferCtx.clearRect(0, 0, this.bufferCanvas.width, this.bufferCanvas.height);
-      this.bufferCtx.drawImage(this.image, offsetX, offsetY, scaledWidth, scaledHeight);
+      bufferCtx.clearRect(0, 0, width, height);
+      bufferCtx.drawImage(this.image, 0, 0, width, height);
       // 绘制已创建矩形
       for (const rect of this.rects) {
-        rect.draw();
+        rect.draw(this.scale);
       }
       // 将缩放后的图片绘制到主画布
-      ctx.drawImage(this.bufferCanvas, 0, 0, canvas.width, canvas.height);
+      ctx.drawImage(this.bufferCanvas, 0, 0, width, height);
+    },
+    // 处理创建标记事件
+    handleCreate() {
+      this.drawing = !this.drawing;
     },
     // 鼠标按下
     handleMouseDown(e) {
-      if (!this.createRect) return;
-      this.currentRect = new Rect(this.bufferCtx, this.dpr, 'rgba(0, 0, 255, 0.3)', e.offsetX, e.offsetY);
+      const mouseX = e.offsetX;
+      const mouseY = e.offsetY;
+      this.prevX = mouseX;
+      this.prevY = mouseY;
+      for(let i = this.rects.length - 1; i > -1; i--) {
+        const rect = this.rects[i];
+        if(rect.isPointInside(mouseX, mouseY)) {
+          this.currentRect = rect;
+          break;
+        }
+      }
+      if(this.currentRect) {
+        this.currentRect.mouseDown(e);
+      }
+      if (!this.drawing) return;
+      // 将离屏画布内容复制到主画布
+      const bufferCtx = this.bufferCanvas.getContext('2d');
+      this.currentRect = new Rect(bufferCtx, this.dpr, e.offsetX, e.offsetY, this.scale);
       this.rects.push(this.currentRect);
     },
     // 鼠标移动
     handleMouseMove(e) {
-      if (!this.createRect) return;
       // 将离屏画布内容复制到主画布
-      const canvas = this.$refs.canvas;
-      const ctx = canvas.getContext('2d');
-      this.currentRect.endX = e.offsetX;
-      this.currentRect.endY = e.offsetY;
+      const ctx = this.canvas.getContext('2d');
+      const bufferCtx = this.bufferCanvas.getContext('2d');
+      if(this.currentRect) {
+        this.currentRect.mouseMove(e, this);
+        // 清除离屏画布并重新绘制
+        bufferCtx.clearRect(0, 0, this.bufferCanvas.width, this.bufferCanvas.height);
+        this.currentRect.draw(); // 绘制当前矩形
+        this.drawCanvas(); // 绘制背景和已有矩形
+
+        ctx.drawImage(this.bufferCanvas, 0, 0);
+      }
+      if (!this.drawing) return;
+      // 清除之前的辅助线
+      ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      ctx.drawImage(this.bufferCanvas, 0, 0);
+      // 获取鼠标在Canvas中的坐标
+      const mouseX = e.offsetX;
+      const mouseY = e.offsetY;
+      // 绘制交叉辅助线
+      ctx.beginPath();
+      ctx.moveTo(mouseX * this.dpr, 0);
+      ctx.lineTo(mouseX * this.dpr, this.canvas.height);
+      ctx.moveTo(0, mouseY * this.dpr);
+      ctx.lineTo(this.canvas.width, mouseY * this.dpr);
+      ctx.strokeStyle = 'red'; // 设置线条颜色
+      ctx.stroke();
+
+      if (!this.currentRect) return;
+      this.currentRect.endX = mouseX;
+      this.currentRect.endY = mouseY;
       // 清除离屏画布并重新绘制
-      this.bufferCtx.clearRect(0, 0, this.bufferCanvas.width, this.bufferCanvas.height);
-      this.drawCanvas(); // 绘制背景和已有矩形
+      bufferCtx.clearRect(0, 0, this.bufferCanvas.width, this.bufferCanvas.height);
       this.currentRect.draw(); // 绘制当前矩形
+      this.drawCanvas(); // 绘制背景和已有矩形
+
       ctx.drawImage(this.bufferCanvas, 0, 0);
     },
     // 鼠标抬起
     handleMouseUp(e) {
-      if (!this.createRect) return;
       // 将离屏画布内容复制到主画布
-      const canvas = this.$refs.canvas;
-      const ctx = canvas.getContext('2d');
+      const ctx = this.canvas.getContext('2d');
+      const bufferCtx = this.bufferCanvas.getContext('2d');
+      if(this.currentRect) {
+        this.currentRect.mouseUp(e);
+        bufferCtx.clearRect(0, 0, this.bufferCanvas.width, this.bufferCanvas.height);
+        this.drawCanvas(); // 绘制背景和已有矩形
+        ctx.drawImage(this.bufferCanvas, 0, 0);
+      }
+      if (!this.drawing) return;
       this.currentRect.endX = e.offsetX;
       this.currentRect.endY = e.offsetY;
       // 保存当前矩形
-      // this.rects.push(this.currentRect);
-      this.createRect = false;
+      this.drawing = false;
       // 最后绘制
-      this.bufferCtx.clearRect(0, 0, this.bufferCanvas.width, this.bufferCanvas.height);
+      bufferCtx.clearRect(0, 0, this.bufferCanvas.width, this.bufferCanvas.height);
       this.drawCanvas(); // 绘制背景和已有矩形
       ctx.drawImage(this.bufferCanvas, 0, 0);
       // 清除 currentRect 以便重新开始绘制新的矩形
@@ -241,14 +299,14 @@ export default {
     zoomIn() {
       if (this.scale < this.maxScale) {
         this.scale = Math.min(this.scale + this.scaleStep, this.maxScale);
-        this.drawCanvas();
+        this.loadImage();
       }
     },
     // 点击缩小按钮
     zoomOut() {
       if (this.scale > this.minScale) {
         this.scale = Math.max(this.scale - this.scaleStep, this.minScale);
-        this.drawCanvas();
+        this.loadImage();
       }
     },
     // 图片切换
@@ -290,12 +348,12 @@ export default {
 /* 布局相关 */
 .container {
   display: flex;
-  height: 100vh; /* 占据整个视口高度 */
+  height: 95vh; /* 占据整个视口高度 */
 }
 
 .sidebar-left,
 .sidebar-right {
-  flex: 0 0 20vw; /* 固定宽度的侧边栏 */
+  flex: 20%; /* 固定宽度的侧边栏 */
   padding: 1vw;
   overflow-y: auto; /* 如果内容溢出，显示滚动条 */
   display: flex;
@@ -303,11 +361,11 @@ export default {
 }
 
 .sidebar-left {
-  flex: 0 0 10vw; /* 固定宽度的侧边栏 */
+  flex: 15%; /* 固定宽度的侧边栏 */
 }
 
 .main-content {
-  flex: 1; /* 占据剩余空间 */
+  flex: 65%; /* 占据剩余空间 */
   height: 94vh;
   margin-top: 3vh;
   display: flex;
--- src/models/Rect.js
+++ src/models/Rect.js
@@ -1,53 +1,167 @@
 export default class Rect {
-  constructor(ctx, dpr, color, startX, startY) {
+  constructor(ctx, dpr, startX, startY, scale) {
     this.ctx = ctx;
     this.dpr = dpr;
-    this.color = color;
+    this.color = 'rgba(0, 0, 255, 0.3)';
     this.startX = startX;
     this.startY = startY;
     this.endX = startX;
     this.endY = startY;
+    this.scale = scale;
+    this.dragging = false;
+    this.resizing = false;
+    this.vertexSize = 10;
+    this.vertexIndex = -1;
   }
+
   get minX() {
     return Math.min(this.startX, this.endX);
   }
+
   get minY() {
     return Math.min(this.startY, this.endY);
   }
+
   get maxX() {
     return Math.max(this.startX, this.endX);
   }
+
   get maxY() {
     return Math.max(this.startY, this.endY);
   }
-  // 绘制矩形
-  draw() {
+
+  /**
+   * 绘制矩形
+   * @param scale 缩放倍率
+   */
+  draw(scale) {
     if (this.minX === this.maxX || this.minY === this.maxY) {
       return;
     }
+    const realScale = 1 / this.scale * scale * this.dpr;
+    const minX = this.minX * realScale;
+    const minY = this.minY * realScale;
+    const maxX = this.maxX * realScale;
+    const maxY = this.maxY * realScale;
     this.ctx.beginPath();
-    this.ctx.moveTo(this.minX * this.dpr, this.minY * this.dpr);
-    this.ctx.lineTo(this.maxX * this.dpr, this.minY * this.dpr);
-    this.ctx.lineTo(this.maxX * this.dpr, this.maxY * this.dpr);
-    this.ctx.lineTo(this.minX * this.dpr, this.maxY * this.dpr);
-    this.ctx.lineTo(this.minX * this.dpr, this.minY * this.dpr);
+    this.ctx.moveTo(minX, minY);
+    this.ctx.lineTo(maxX, minY);
+    this.ctx.lineTo(maxX, maxY);
+    this.ctx.lineTo(minX, maxY);
+    this.ctx.lineTo(minX, minY);
     this.ctx.fillStyle = this.color;
     this.ctx.strokeStyle = "#fff";
     this.ctx.lineWidth = 1;
     this.ctx.lineCap = 'square';
     this.ctx.fill();
     this.ctx.stroke();
-  }
-  // 点击事件
-  onClick(event) {
-    console.log('Rectangle clicked!', event);
-  }
-  //
-  onMouseEnter(event) {
-    console.log('Mouse entered rectangle!', event);
+    // 绘制四个顶点
+    this.drawVertex(minX, minY);
+    this.drawVertex(maxX, minY);
+    this.drawVertex(maxX, maxY);
+    this.drawVertex(minX, maxY);
   }
 
-  onMouseLeave(event) {
-    console.log('Mouse left rectangle!', event);
+  /**
+   * 绘制矩形顶点，坐标(x, y)
+   * @param x
+   * @param y
+   */
+  drawVertex(x, y) {
+    if(this.dragging || this.resizing) {
+      this.ctx.fillStyle = '#FF4500'; // 拖动或缩放状态，红色顶点
+    } else {
+      this.ctx.fillStyle = '#A7FC00'; // 正常状态，青色顶点
+    }
+    this.ctx.fillRect(x - this.vertexSize / 2, y - this.vertexSize / 2, this.vertexSize, this.vertexSize);
+  }
+
+  /**
+   * 判断坐标(x, y)是否在矩形内部
+   * @param x
+   * @param y
+   */
+  isPointInside(x, y) {
+    return x >= this.startX && x <= this.endX && y >= this.startY && y <= this.endY;
+  }
+
+  /**
+   * 判断坐标(x, y)是否在矩形顶点内部
+   * @param x
+   * @param y
+   */
+  isPointInsideVertex(x, y) {
+    const vertices = [
+      {x: this.startX, y: this.startY},
+      {x: this.endX, y: this.startY},
+      {x: this.endX, y: this.endY},
+      {x: this.startX, y: this.endY}
+    ];
+    for (let i = 0; i < vertices.length; i++) {
+      const vx = vertices[i].x;
+      const vy = vertices[i].y;
+      if (x >= vx - this.vertexSize / 2 && x <= vx + this.vertexSize / 2 &&
+        y >= vy - this.vertexSize / 2 && y <= vy + this.vertexSize / 2) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  // 鼠标按下事件处理
+  mouseDown(e) {
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+    this.vertexIndex = this.isPointInsideVertex(mouseX, mouseY);
+    if(this.vertexIndex !== -1) {
+      this.resizing = true;
+    } else if(this.isPointInside(mouseX, mouseY)) {
+      this.dragging = true;
+    }
+  }
+
+  // 鼠标移动事件处理
+  mouseMove(e, that) {
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+    if (this.dragging) {
+      // 拖动矩形
+      const disX = mouseX - that.prevX;
+      const disY = mouseY - that.prevY;
+      this.startX += disX;
+      this.startY += disY;
+      this.endX += disX;
+      this.endY += disY;
+      that.prevX += disX;
+      that.prevY += disY;
+    }
+    if (this.resizing) {
+      // 缩放矩形
+      switch (this.vertexIndex) {
+        case 0: // 左上角顶点
+          this.startX = mouseX;
+          this.startY = mouseY;
+          break;
+        case 1: // 右上角顶点
+          this.endX = mouseX;
+          this.startY = mouseY;
+          break;
+        case 2: // 右下角顶点
+          this.endX = mouseX;
+          this.endY = mouseY;
+          break;
+        case 3: // 左下角顶点
+          this.startX = mouseX;
+          this.endY = mouseY;
+          break;
+      }
+    }
+    this.draw();
+  }
+
+  // 鼠标松开事件处理
+  mouseUp(e, that) {
+    this.dragging = false;
+    this.resizing = false;
   }
 }
